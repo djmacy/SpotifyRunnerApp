2d4254302fda6ca0947108b7a936c1b4a42d65da
David Macy <dmacy@carroll.edu>
2024-11-01 20:27:28 -0600

    created postgresql db for the web app

---
+++ Models/SpotifyUser.cs
@@ -0,0 +1,11 @@
+﻿namespace SpotifyRunnerApp.Models
+{
+    public class SpotifyUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string AccessToken { get; set; }
+        public string ExpiresIn { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
